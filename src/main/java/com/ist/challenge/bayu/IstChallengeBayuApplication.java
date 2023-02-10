3a0d48439fdaa1028214f6624e7aa5a3fee560ae
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-10 10:50:54 +0700

    initial commit

---
+++ src/main/java/com/ist/challenge/bayu/IstChallengeBayuApplication.java
@@ -0,0 +1,13 @@
+package com.ist.challenge.bayu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IstChallengeBayuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IstChallengeBayuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ist/challenge/bayu/IstChallengeBayuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ist.challenge.bayu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IstChallengeBayuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
