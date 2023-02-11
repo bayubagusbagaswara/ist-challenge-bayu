9aea628524a88e48d82dbb351de5322928581abf
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-11 19:57:10 +0700

    implement method register and login

--- src/main/java/com/ist/challenge/bayu/controller/AuthController.java
+++ src/main/java/com/ist/challenge/bayu/controller/AuthController.java
@@ -1,5 +1,11 @@
 package com.ist.challenge.bayu.controller;
 
+import com.ist.challenge.bayu.dto.*;
+import com.ist.challenge.bayu.service.AuthService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +13,34 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/auth")
 public class AuthController {
 
-    // register
+    private final AuthService authService;
 
-    // login
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<MessageResponse> register(@RequestBody RegisterRequest registerRequest) {
+        RegisterResponse register = authService.register(registerRequest);
+        MessageResponse messageResponse = MessageResponse.builder()
+                .code(201)
+                .success(Boolean.TRUE)
+                .message("Register berhasil dengan username : " + register.getUsername())
+                .build();
+
+        return new ResponseEntity<>(messageResponse, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<MessageResponse> login(@RequestBody LoginRequest loginRequest) {
+        LoginResponse login = authService.login(loginRequest);
+        MessageResponse messageResponse = MessageResponse.builder()
+                .code(200)
+                .success(Boolean.TRUE)
+                .message("Login berhasil dengan username : " + login.getUsername())
+                .build();
+
+        return new ResponseEntity<>(messageResponse, HttpStatus.OK);
+    }
+
 }
