629c51f62c1e49ecba6cd7c95783fd3663d50205
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-11 20:47:35 +0700

    update all method

--- src/main/java/com/ist/challenge/bayu/service/impl/AuthServiceImpl.java
+++ src/main/java/com/ist/challenge/bayu/service/impl/AuthServiceImpl.java
@@ -2,37 +2,35 @@ package com.ist.challenge.bayu.service.impl;
 
 import com.ist.challenge.bayu.dto.*;
 import com.ist.challenge.bayu.exception.BadRequestException;
-import com.ist.challenge.bayu.exception.ResourceNotFoundException;
 import com.ist.challenge.bayu.exception.UnauthorizedException;
-import com.ist.challenge.bayu.model.User;
-import com.ist.challenge.bayu.repository.UserRepository;
 import com.ist.challenge.bayu.service.AuthService;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import com.ist.challenge.bayu.service.UserService;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthServiceImpl implements AuthService {
 
+    private final UserService userService;
 
-    private final UserRepository userRepository;
-    private final PasswordEncoder passwordEncoder;
-
-    public AuthServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-        this.userRepository = userRepository;
-        this.passwordEncoder = passwordEncoder;
+    public AuthServiceImpl(UserService userService) {
+        this.userService = userService;
     }
 
     @Override
     public RegisterResponse register(RegisterRequest registerRequest) {
-        User user = new User();
-        user.setUsername(registerRequest.getUsername());
-        user.setPassword(registerRequest.getPassword());
 
-        userRepository.save(user);
+        CreateUserRequest createUserRequest = CreateUserRequest.builder()
+                .username(registerRequest.getUsername())
+                .password(registerRequest.getPassword())
+                .build();
+
+        CreateUserResponse createUserResponse = userService.createUser(createUserRequest);
+
         return RegisterResponse.builder()
-                .id(user.getId())
-                .username(user.getUsername())
-                .password(user.getPassword())
+                .id(createUserResponse.getId())
+                .username(createUserRequest.getUsername())
+                .password(createUserResponse.getPassword())
                 .build();
     }
 
@@ -51,22 +49,20 @@ public class AuthServiceImpl implements AuthService {
     public void checkUsernameOrPasswordIsEmpty(LoginRequest loginRequest) {
         if (loginRequest.getUsername().equalsIgnoreCase("")
                 || loginRequest.getUsername().isEmpty()) {
-            throw new BadRequestException(new MessageResponse(400, Boolean.FALSE, "Username is empty"));
+            throw new BadRequestException(new MessageResponse(HttpStatus.BAD_REQUEST.value(), Boolean.FALSE, "Username is empty"));
         }
 
         if (loginRequest.getPassword().equalsIgnoreCase("")
                 || loginRequest.getPassword().isEmpty()) {
-            throw new BadRequestException(new MessageResponse(400, Boolean.FALSE, "Password is empty"));
+            throw new BadRequestException(new MessageResponse(HttpStatus.BAD_REQUEST.value(), Boolean.FALSE, "Password is empty"));
         }
     }
 
-    // check username and password is matching
     public void checkUsernameAndPasswordIsMatching(LoginRequest loginRequest) {
-        User user = userRepository.findByUsername(loginRequest.getUsername())
-                .orElseThrow(() -> new ResourceNotFoundException(new MessageResponse(404, Boolean.FALSE, "User not found with username : " + loginRequest.getUsername())));
+        UserResponse user = userService.getUserByUsername(loginRequest.getUsername());
 
         if (!user.getPassword().equalsIgnoreCase(loginRequest.getPassword())) {
-            throw new UnauthorizedException(new MessageResponse(401, Boolean.FALSE, "Password does not match"));
+            throw new UnauthorizedException(new MessageResponse(HttpStatus.UNAUTHORIZED.value(), Boolean.FALSE, "Password does not match"));
         }
     }
 
