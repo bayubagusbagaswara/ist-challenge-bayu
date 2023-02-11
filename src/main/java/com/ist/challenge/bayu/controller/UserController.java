dd8b39be44684e661b49fb7a584ec4c43636813c
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-11 10:23:31 +0700

    add comment

--- src/main/java/com/ist/challenge/bayu/controller/UserController.java
+++ src/main/java/com/ist/challenge/bayu/controller/UserController.java
@@ -6,4 +6,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    // create new user
+
+    // get user by id
+
+    // get all user
+
+    // get all user by pagination and sorting
+
+    // update user
+
+    // delete user
 }
