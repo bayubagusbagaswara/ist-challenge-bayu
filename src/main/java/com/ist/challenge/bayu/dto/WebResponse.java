c8234d73b297ffc351c92ffe5784b50074529d2e
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-11 19:32:01 +0700

    add dto WebResponse

---
+++ src/main/java/com/ist/challenge/bayu/dto/WebResponse.java
@@ -0,0 +1,39 @@
+package com.ist.challenge.bayu.dto;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@Builder
+@JsonPropertyOrder({
+        "code",
+        "success",
+        "message",
+        "data"
+})
+@AllArgsConstructor
+@NoArgsConstructor
+public class WebResponse<T> {
+
+    @JsonProperty("code")
+    private Integer code;
+
+    @JsonProperty("success")
+    private Boolean success;
+
+    @JsonProperty("message")
+    private String message;
+
+    @JsonProperty("data")
+    private T data;
+
+    @JsonIgnore
+    private HttpStatus status;
+
+}
