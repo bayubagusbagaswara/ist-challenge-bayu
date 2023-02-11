37b3da66f42373dac7eb16094ae9356c0f6abfeb
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-11 10:49:31 +0700

    add service implementation UserServiceImpl

---
+++ src/main/java/com/ist/challenge/bayu/service/impl/UserServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ist.challenge.bayu.service.impl;
+
+import com.ist.challenge.bayu.service.UserService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserServiceImpl implements UserService {
+}
