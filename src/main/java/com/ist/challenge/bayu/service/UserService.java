1ce728d019bb0e171159e0d5b88d485fb32ebfdc
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-11 20:18:54 +0700

    add method contract createUser

--- src/main/java/com/ist/challenge/bayu/service/UserService.java
+++ src/main/java/com/ist/challenge/bayu/service/UserService.java
@@ -1,4 +1,20 @@
 package com.ist.challenge.bayu.service;
 
+import com.ist.challenge.bayu.dto.CreateUserRequest;
+import com.ist.challenge.bayu.dto.CreateUserResponse;
+
 public interface UserService {
+
+    // create new user
+    CreateUserResponse createUser(CreateUserRequest createUserRequest);
+
+    // get user by id
+
+    // get all user
+
+    // get all user by pagination and sorting
+
+    // update user
+
+    // delete user
 }
