2767d9a69060216803e766abdc982a65821df367
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-11 10:49:22 +0700

    add service implementation AuthServiceImpl

---
+++ src/main/java/com/ist/challenge/bayu/service/impl/AuthServiceImpl.java
@@ -0,0 +1,8 @@
+package com.ist.challenge.bayu.service.impl;
+
+import com.ist.challenge.bayu.service.AuthService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+}
