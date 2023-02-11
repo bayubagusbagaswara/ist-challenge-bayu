00b1794a054b5aa7a076ece4a55d347c04c8cece
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-11 20:23:25 +0700

    remove all comment

--- src/main/java/com/ist/challenge/bayu/service/UserService.java
+++ src/main/java/com/ist/challenge/bayu/service/UserService.java
@@ -6,21 +6,16 @@ import java.util.List;
 
 public interface UserService {
 
-    // create new user
     CreateUserResponse createUser(CreateUserRequest createUserRequest);
 
-    // get user by id
     UserResponse getUserById(Long id);
 
-    // get all user
     List<UserResponse> getAllUsers();
 
-    // get all user by pagination and sorting
     ListUserResponse listAllUsers(ListUserRequest listUserRequest);
 
-    // update user
     UpdateUserResponse updateUser(Long id, UpdateUserRequest updateUserRequest);
 
-    // delete user
     void deleteUserById(Long id);
+
 }
