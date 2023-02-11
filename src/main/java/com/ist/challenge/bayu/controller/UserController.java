322d0c87a4a08e8cecedd109b94fe624c09b629c
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-11 20:18:43 +0700

    update UserController

--- src/main/java/com/ist/challenge/bayu/controller/UserController.java
+++ src/main/java/com/ist/challenge/bayu/controller/UserController.java
@@ -1,11 +1,18 @@
 package com.ist.challenge.bayu.controller;
 
+import com.ist.challenge.bayu.service.UserService;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     // create new user
 
