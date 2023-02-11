8bc2c0938f76187ea22209f32f946f82c594c9de
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-11 10:22:19 +0700

    added controller UserController

---
+++ src/main/java/com/ist/challenge/bayu/controller/UserController.java
@@ -0,0 +1,9 @@
+package com.ist.challenge.bayu.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+}
