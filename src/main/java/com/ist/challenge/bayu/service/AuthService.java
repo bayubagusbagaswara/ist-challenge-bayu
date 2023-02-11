fbfc9e467755de5744ba6c71bef7289d507e5f33
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-11 14:14:38 +0700

    add method register

--- src/main/java/com/ist/challenge/bayu/service/AuthService.java
+++ src/main/java/com/ist/challenge/bayu/service/AuthService.java
@@ -1,8 +1,12 @@
 package com.ist.challenge.bayu.service;
 
+import com.ist.challenge.bayu.dto.RegisterRequest;
+import com.ist.challenge.bayu.dto.RegisterResponse;
+
 public interface AuthService {
 
     // register
+    RegisterResponse register(RegisterRequest registerRequest);
 
     // login
 }
