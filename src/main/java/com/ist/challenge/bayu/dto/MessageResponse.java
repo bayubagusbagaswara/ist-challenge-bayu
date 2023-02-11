82761dbf102c7fbb9de476f8c9d82dcb42d22c94
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-11 19:56:38 +0700

    update dto MessageResponse

--- src/main/java/com/ist/challenge/bayu/dto/MessageResponse.java
+++ src/main/java/com/ist/challenge/bayu/dto/MessageResponse.java
@@ -3,10 +3,12 @@ package com.ist.challenge.bayu.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
+@Builder
 @JsonPropertyOrder({
         "code",
         "success",
