62eea6c991dfcc12123b1a4a265ca4e80ba3a5e4
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-11 20:19:16 +0700

    add method contract getUserById

--- src/main/java/com/ist/challenge/bayu/service/UserService.java
+++ src/main/java/com/ist/challenge/bayu/service/UserService.java
@@ -2,6 +2,7 @@ package com.ist.challenge.bayu.service;
 
 import com.ist.challenge.bayu.dto.CreateUserRequest;
 import com.ist.challenge.bayu.dto.CreateUserResponse;
+import com.ist.challenge.bayu.dto.UserResponse;
 
 public interface UserService {
 
@@ -9,6 +10,7 @@ public interface UserService {
     CreateUserResponse createUser(CreateUserRequest createUserRequest);
 
     // get user by id
+    UserResponse getUserById(Long id);
 
     // get all user
 
