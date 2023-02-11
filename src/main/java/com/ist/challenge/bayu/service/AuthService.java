a06cc4aa0832362f5b443eb8c21e7b17d8a20ef7
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-11 20:17:29 +0700

    update AuthService

--- src/main/java/com/ist/challenge/bayu/service/AuthService.java
+++ src/main/java/com/ist/challenge/bayu/service/AuthService.java
@@ -4,9 +4,7 @@ import com.ist.challenge.bayu.dto.*;
 
 public interface AuthService {
 
-    // register
     RegisterResponse register(RegisterRequest registerRequest);
 
-    // login
-    MessageResponse login(LoginRequest loginRequest);
+    LoginResponse login(LoginRequest loginRequest);
 }
