dcc686b977b6c0e300d3740807ead657cb24f9c5
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-12 15:02:53 +0700

    update ErrorController

--- src/main/java/com/ist/challenge/bayu/controller/ErrorController.java
+++ src/main/java/com/ist/challenge/bayu/controller/ErrorController.java
@@ -32,32 +32,27 @@ public class ErrorController {
         return new ResponseEntity<>(new ApiResponse<>(HttpStatus.BAD_REQUEST.value(), Boolean.FALSE, "Bad Request Validation", errors), HttpStatus.BAD_REQUEST);
     }
 
-    // 400
     @ExceptionHandler(value = BadRequestException.class)
     @ResponseStatus(code = HttpStatus.BAD_REQUEST)
     public ResponseEntity<MessageResponse> badRequestException(BadRequestException exception) {
-        return new ResponseEntity<>(new MessageResponse(HttpStatus.BAD_REQUEST.value(), Boolean.FALSE, exception.getMessage()), HttpStatus.BAD_REQUEST);
+        return new ResponseEntity<>(exception.getMessageResponse(), HttpStatus.BAD_REQUEST);
     }
 
-    // 401
     @ExceptionHandler(UnauthorizedException.class)
     @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
     public ResponseEntity<MessageResponse> unauthorizedException(UnauthorizedException exception) {
-        return new ResponseEntity<>(new MessageResponse(HttpStatus.UNAUTHORIZED.value(), Boolean.FALSE, exception.getMessage()), HttpStatus.UNAUTHORIZED);
+        return new ResponseEntity<>(exception.getMessageResponse(), HttpStatus.UNAUTHORIZED);
     }
 
-    // 404
     @ExceptionHandler(value = ResourceNotFoundException.class)
     @ResponseStatus(code = HttpStatus.NOT_FOUND)
     public ResponseEntity<MessageResponse> resourceNotFoundHandler(ResourceNotFoundException exception) {
-        return new ResponseEntity<>(new MessageResponse(HttpStatus.NOT_FOUND.value(), Boolean.FALSE, exception.getMessage()), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(exception.getMessageResponse(), HttpStatus.NOT_FOUND);
     }
 
-    // 409
     @ExceptionHandler(value = ConflictException.class)
     @ResponseStatus(code = HttpStatus.CONFLICT)
     public ResponseEntity<MessageResponse> conflictException(ConflictException exception) {
-//        return new ResponseEntity<>(new MessageResponse(HttpStatus.CONTINUE.value(), Boolean.FALSE, exception.getMessage()), HttpStatus.CONFLICT);
         return new ResponseEntity<>(exception.getApiResponse(), HttpStatus.CONFLICT);
     }
 
