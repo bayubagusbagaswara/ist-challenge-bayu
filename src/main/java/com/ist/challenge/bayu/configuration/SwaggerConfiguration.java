b852ff76aa5afd41640d4644d55672bb94391377
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-02-13 10:17:17 +0700

    add SwaggerConfiguration

---
+++ src/main/java/com/ist/challenge/bayu/configuration/SwaggerConfiguration.java
@@ -0,0 +1,37 @@
+package com.ist.challenge.bayu.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfiguration {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.ist.challenge.bayu"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        ApiInfo apiInfo = new ApiInfo(
+                "My Demo API",
+                "This is demo Login and Registration API",
+                "API TOS",
+                "Terms of service",
+                new Contact("Bayu Bagaswara", "www.")
+        );
+        return apiInfo;
+    }
+}
